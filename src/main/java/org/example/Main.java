c682674034f129689ba8c6ef0c4de6222ba9d3bb
Kirtsuha <batukhtin.kir@gmail.com>
2026-02-15 16:35:43 +0300

    fixed configuration class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.configuration.AppConfig;
+import org.example.configuration.HibernateConfiguration;
 import org.example.service.OperationsConsoleListener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -7,7 +9,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                AppConfig.class,
+                HibernateConfiguration.class
+        );
         OperationsConsoleListener consoleListener = applicationContext.getBean(OperationsConsoleListener.class);
         consoleListener.logic_switcher();
     }
